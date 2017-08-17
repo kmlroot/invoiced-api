c9617fdc5819f1717f0753e6acf558e6272b6665
Mauricio Serna <mauricioserf@gmail.com>
2017-08-17 16:10:51 -0500

    [Add] create user

---
+++ app/controllers/v1/users_controller.rb
@@ -0,0 +1,21 @@
+module V1
+  class UsersController < ApplicationController
+    def create
+      @user = User.new(user_params)
+
+      binding.pry
+
+      if @user.save
+        render :create
+      else
+        head(:unprocessable_entity)
+      end
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   namespace :v1, defaults: { format: :json } do
     resources :contacts
     resource :sessions, only: [:create, :destroy]
+    resources :users, only: [:create]
   end
 end
