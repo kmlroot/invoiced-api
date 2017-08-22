ec67d6b5495dbd557ac910c356ed43458e198055
Mauricio Serna <mauricioserf@gmail.com>
2017-08-21 23:04:26 -0500

    [Add] creating account endpoints.

---
+++ app/controllers/v1/accounts_controller.rb
@@ -0,0 +1,35 @@
+module V1
+  class AccountsController < ApplicationController
+    def create
+      @account = current_user.accounts.build(account_params)
+
+      if @account.save
+        render :create, status: :created
+      else
+        head(:unprocessable_entity)
+      end
+    end
+
+    def update
+      @account = current_user.accounts.friendly.find(params[:account_id])
+
+      if @account.update(account_params)
+        render :update
+      else
+        head(:unprocessable_entity)
+      end
+    end
+
+    private
+
+    def account_params
+      params.require(:account).permit(
+        :name,
+        :address,
+        :vat_rate,
+        :tax_payer_id,
+        :default_currency
+      )
+    end
+  end
+end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,4 @@
 class Contact < ApplicationRecord
   belongs_to :organization
+  belongs_to :account
 end
